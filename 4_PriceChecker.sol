24c62f5aff80f24fddfc1c1dd307fc8b59c9e127
Kev_n <98072108+itmekevin@users.noreply.github.com>
2022-11-07 00:30:57 -0600

    Update 4_PriceChecker.sol
    commented unnecessary variables. Updated reference addresses to Goerli network.

--- 4_PriceChecker.sol
+++ 4_PriceChecker.sol
@@ -10,11 +10,13 @@ contract BTC_Oracle_Test {
     AggregatorV3Interface internal priceFeedETH;
 
     /**
-     * Network: Mainnet
+     * Network: Goerli
+     * Aggregator: BTC/USD
+     * Address: 0x6135b13325bfC4B00278B4abC5e20bbce2D6580e
      */
     constructor() {
-        priceFeed = AggregatorV3Interface(0xf4030086522a5beea4988f8ca5b36dbc97bee88c);
-        priceFeedETH = AggregatorV3Interface(0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419);
+        priceFeed = AggregatorV3Interface(0xA39434A63A52E749F02807ae27335515BA4b07F7);
+        priceFeedETH = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
     }
 
     /**
@@ -22,22 +24,22 @@ contract BTC_Oracle_Test {
      */
     function getLatestPriceBTC() public view returns (int) {
         (
-            uint80 roundID, 
+            /*uint80 roundID*/, 
             int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
         ) = priceFeed.latestRoundData();
         return price;
     }
 
         function getLatestPriceETH() public view returns (int) {
         (
-            uint80 roundID, 
+            /*uint80 roundID*/, 
             int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
         ) = priceFeedETH.latestRoundData();
         return price;
     }
